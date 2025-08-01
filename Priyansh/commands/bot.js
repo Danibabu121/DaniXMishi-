f9d96543e971a361dca1121ab3b8c2aad93e61ae
Danibabu121 <babudani198@gmail.com>
2025-08-01 22:54:21 +0500

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝗔𝗿𝘂𝗻 𝗞𝘂𝗺𝗮𝗿🌹 `
+      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : ‣⃟ ⃝😍ɭɵ͡𝐯̽ɛ̽͜͜͡͡ɼʂ‣⃟ ⃝😙𓆩๏̬̬̬̬̬̬̬̬̬̬𝐃ɑ͢ƞͯı⃪𝆭͜͡iʂⴙ ꭗ 𝐌ı⃪𝆭͜͡iʂⴙı⃪𝆭͜͡iᏇ𓆪٭༑͡༑ﮩـ😗‣⃟👑 `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
