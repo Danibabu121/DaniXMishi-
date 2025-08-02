cbff7d06769c4480281db82fdf53d08041e6a878
Danibabu121 <babudani198@gmail.com>
2025-08-02 17:40:26 +0500

    Update Mishi1.js

--- Priyansh/commands/Arun1.js
+++ Priyansh/commands/Arun1.js
@@ -1,46 +1,29 @@
+const fs = require("fs");
 module.exports.config = {
-  name: "Liones",
-  version: "1.1.1",
-  hasPermssion: 0,
-  credits: "Liones",
-  description: "Just Respond",
-  commandCategory: "no prefix",
-  cooldowns: 5,
+	name: "Mishi",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Arun", 
+	description: "no prefix",
+	commandCategory: "No command marks needed",
+	usages: "...",
+    cooldowns: 100, 
 };
 
-module.exports.handleEvent = function({ api, event }) {
-  const { threadID, messageID, body } = event;
-  const react = body.toLowerCase();
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("owner") ||
+     react.includes("Mishi") || 
+react.includes("Dani")) {
+		var msg = {
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐃𝐚𝐧𝐢 𝐗 𝐌𝐢𝐬𝐡𝐢✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKal ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/groups/207371140648761/?ref=share_group_link`",
+				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  if (
-    react.includes("liones") ||
-    react.includes("sherni") ||
-    react.includes("lione") ||
-    react.includes("owner") ||
-    react.includes("sher") ||
-    react.includes("admin")
-  ) {
-    const msg = {
-      body: `★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★
-★𝗗𝗮𝗻𝗶 𝗫 𝗠𝗶𝘀𝗵𝗶♡★
-
-☞𝗗𝗮𝗻𝗶 𝗫 𝗠𝗶𝘀𝗵𝗶♡:
-
-میــں محـبتـوں کـے ســودے کـچھ یــوں کـرتـی ہـوں🦋
-بــس مسکراتــی ہــوں اور دل خـریــد لیتـی ہوں ❤️`,
-      attachment: [
-        api.sendMessage({
-          attachment: [
-            // Add ibb direct image URL here
-            api.resolvePhotoUrl("https://i.ibb.co/99pSZhbT/received-1295330775646213.jpg")
-          ]
-        })
-      ]
-    };
-
-    api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😘", messageID, () => {}, true);
   }
-};
-
-module.exports.run = function() {};
