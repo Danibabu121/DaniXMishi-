e6874872bc6c14ed8d5fef76c45e0fde7d708713
Danibabu121 <babudani198@gmail.com>
2025-08-02 16:07:55 +0500

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -1,51 +1,76 @@
 module.exports.config = {
-	name: "inf",
-	version: "1.0.1", 
-	hasPermssion: 0,
-	credits: "Arun Kumar", //don't change the credits please
-	description: "Admin and Bot info.",
-	commandCategory: "info",
-	cooldowns: 1,
-	dependencies: 
-	{
-    "request":"",
-    "fs-extra":"",
-    "axios":""
+  name: "inf",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "Arun Kumar", // Don't change credits
+  description: "Admin and Bot info.",
+  commandCategory: "info",
+  cooldowns: 1,
+  dependencies: {
+    "axios": "",
+    "fs-extra": "",
+    "request": "",
+    "moment-timezone": ""
   }
 };
-module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
-const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Kolkata").format("『D/MM/YYYY』 【HH:mm:ss】");
-var link =                                     
-["https://i.postimg.cc/4yVw6tm7/Picsart-23-03-26-11-08-19-025.jpg", "https://i.imgur.com/rg0fjQE.jpg", "https://i.imgur.com/QcNXYfT.jpg", "https://i.imgur.com/WhVSHLB.png"];
-var callback = () => api.sendMessage({body:`🌹𝙰𝙳𝙼𝙸𝙽 𝙰𝙽𝙳 𝙱𝙾𝚃 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽 🇮🇳 
 
+module.exports.run = async function ({ api, event, args }) {
+  const axios = require("axios");
+  const request = require("request");
+  const fs = require("fs-extra");
+  const moment = require("moment-timezone");
+
+  const time = process.uptime();
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = Math.floor(time % 60);
+
+  const juswa = moment.tz("Asia/Kolkata").format("『D/MM/YYYY』 【HH:mm:ss】");
+
+  const images = [
+    "https://i.postimg.cc/4yVw6tm7/Picsart-23-03-26-11-08-19-025.jpg",
+    "https://i.imgur.com/rg0fjQE.jpg",
+    "https://i.imgur.com/QcNXYfT.jpg",
+    "https://i.imgur.com/WhVSHLB.png"
+  ];
+
+  const selectedImage = images[Math.floor(Math.random() * images.length)];
+  const imgPath = __dirname + "/cache/juswa.jpg";
+
+  try {
+    const callback = () => {
+      api.sendMessage({
+        body: `🌹𝙰𝙳𝙼𝙸𝙽 𝙰𝙽𝙳 𝙱𝙾𝚃 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽 🇮🇳
 
 ☄️𝗕𝗢𝗧 𝗡𝗔𝗠𝗘☄️ ⚔ ${global.config.BOTNAME} ⚔
 
-🔥𝗢𝗪𝗡𝗘𝗥 🔥☞︎︎︎ 𝙰𝚛𝚞𝚗 𝙺𝚞𝚖𝚊𝚛 ☜︎︎︎✰ \n\n
-🙈🄾🅆🄽🄴🅁 🄲🄾🄽🅃🄰🄲🅃 🄻🄸🄽🄺🅂🙈➪ \n\n  𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 🧨https://www.facebook.com/arun.x76 💞🕊️
-  \n 
-✅𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘👉 @arunkumar_031 \n\n  ====𝗧𝗼 𝗹𝗲𝗮𝗿𝗻 𝗛𝗼𝘄 𝘁𝗼 𝗖𝗿𝗲𝗮𝘁𝗲 𝗔 𝗯𝗼𝘁 === 𝗩𝗶𝘀𝗶𝘁 𝗔𝗻𝗱 𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗧𝗼 𝗠𝘆 𝗖𝗵𝗮𝗻𝗻𝗲𝗹✅ 🗡 https://www.youtube.com/@mirrykal
+🔥𝗢𝗪𝗡𝗘𝗥 🔥☞︎︎︎ 𝙰𝚛𝚞𝚗 𝙺𝚞𝚖𝚊𝚛 ☜︎︎︎✰
+
+🙈🄾🅆🄽🄴🅁 🄲🄾🄽🅃🄰🄲🅃 🄻🄸🄽🄺🅂🙈➪
+
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 🧨 https://www.facebook.com/arun.x76 💞🕊️
+𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠 👉 @arunkumar_031
+
+====𝗛𝗼𝘄 𝘁𝗼 𝗖𝗿𝗲𝗮𝘁𝗲 𝗔 𝗯𝗼𝘁 ====
+𝗩𝗶𝘀𝗶𝘁 𝗮𝗻𝗱 𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 ➤ https://www.youtube.com/@mirrykal
+
 ✧══════•❁❀❁•══════✧
 
-🌸𝗕𝗼𝘁 𝗣𝗿𝗲𝗳𝗶𝘅🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
+🌸𝗕𝗼𝘁 𝗣𝗿𝗲𝗳𝗶𝘅🌸 ➤ ${global.config.PREFIX}
 
-🥳UPTIME🥳
+🥳 𝗧𝗼𝗱𝗮𝘆'𝘀 𝗗𝗮𝘁𝗲 & 𝗧𝗶𝗺𝗲 🥳 ➤ ${juswa}
 
-🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
+⚡ 𝗕𝗼𝘁 𝗨𝗽𝘁𝗶𝗺𝗲 ⚡ ➤ ${hours}:${minutes}:${seconds}
 
-⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
+✅ 𝗧𝗵𝗮𝗻𝗸𝘀 𝗳𝗼𝗿 𝘂𝘀𝗶𝗻𝗴 ${global.config.BOTNAME} ❤🖤`,
+        attachment: fs.createReadStream(imgPath)
+      }, event.threadID, () => fs.unlinkSync(imgPath));
+    };
 
-✅Thanks for using My Bot ❤ ${global.config.BOTNAME} 🖤
+    request(encodeURI(selectedImage)).pipe(fs.createWriteStream(imgPath)).on("close", callback);
 
-`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/juswa.jpg")).on("close",() => callback());
-   };
+  } catch (err) {
+    console.log("❌ Error in inf command:", err);
+    return api.sendMessage("⚠️ An error occurred while fetching bot info.", event.threadID);
+  }
+};
