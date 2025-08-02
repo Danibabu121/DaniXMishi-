1bc3c1616c8b5e73b60cf9720a920b829bf82566
Danibabu121 <babudani198@gmail.com>
2025-08-02 17:29:39 +0500

    Update Mishi1.js

--- Priyansh/commands/Arun1.js
+++ Priyansh/commands/Arun1.js
@@ -1,38 +1,46 @@
-const fs = require("fs");
 module.exports.config = {
-	name: "DanixMishi",
-    version: "1.1.1",
-	hasPermssion: 0,
-	credits: "Liones", 
-	description: "Just Respond",
-	commandCategory: "no prefix",
-    cooldowns: 5, 
+  name: "Liones",
+  version: "1.1.1",
+  hasPermssion: 0,
+  credits: "Liones",
+  description: "Just Respond",
+  commandCategory: "no prefix",
+  cooldowns: 5,
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	let react = event.body.toLowerCase();
-	if(react.includes("liones") ||
-     react.includes("sherni") || react.includes("lione") || react.includes("Owner") ||
-react.includes("sher") ||
-react.includes("admin")) {
-		var msg = {
-				body: `★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★
-               ★𝗗𝗮𝗻𝗶 𝗫 𝗠𝗶𝘀𝗵𝗶♡★
+module.exports.handleEvent = function({ api, event }) {
+  const { threadID, messageID, body } = event;
+  const react = body.toLowerCase();
 
+  if (
+    react.includes("liones") ||
+    react.includes("sherni") ||
+    react.includes("lione") ||
+    react.includes("owner") ||
+    react.includes("sher") ||
+    react.includes("admin")
+  ) {
+    const msg = {
+      body: `★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★
+★𝗗𝗮𝗻𝗶 𝗫 𝗠𝗶𝘀𝗵𝗶♡★
 
 ☞𝗗𝗮𝗻𝗶 𝗫 𝗠𝗶𝘀𝗵𝗶♡:
 
-*میــں محـبتـوں کـے ســودے کـچھ یــوں کـرتـی ہـوں🦋
-بــس مسکراتــی ہــوں اور دل خـریــد لیتـی ہوں ❤️*  
+میــں محـبتـوں کـے ســودے کـچھ یــوں کـرتـی ہـوں🦋
+بــس مسکراتــی ہــوں اور دل خـریــد لیتـی ہوں ❤️`,
+      attachment: [
+        api.sendMessage({
+          attachment: [
+            // Add ibb direct image URL here
+            api.resolvePhotoUrl("https://i.ibb.co/99pSZhbT/received-1295330775646213.jpg")
+          ]
+        })
+      ]
+    };
 
+    api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😘", messageID, () => {}, true);
+  }
+};
 
-★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 : 𝗗𝗮𝗻𝗶 𝗫 𝗠𝗶𝘀𝗵𝗶♡`,attachment: fs.createReadStream(__dirname + `/noprefix/lion.jpg`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😘", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
-
-	}
+module.exports.run = function() {};
