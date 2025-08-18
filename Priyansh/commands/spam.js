2b7d7fed26613413e5a2217353558df1fbde148c
Danibabu121 <babudani198@gmail.com>
2025-08-18 18:05:53 +0500

    Create spam.js

---
+++ Priyansh/commands/spam.js
@@ -0,0 +1,24 @@
+module.exports.config = {
+name: "spam",
+	version: "Beta",
+	hasPermssion: 2,
+	credits: "𝒁𝒂𝑳𝒊𝒎 𝑯𝒂𝑺𝒆𝑬𝒏𝒂",
+	description: "spam osn the Chat box",
+	commandCategory: "test",
+	usages: "spam",
+	cooldowns: 5,
+	dependencies: "",
+};
+
+module.exports.run = function ({ api, event, Users }) {
+	var { threadID, messageID } = event;
+	var k = function (k) { api.sendMessage(k, threadID)};
+
+	//*vonglap
+	
+for (i = 0; i < 200; i++) {
+ k("🎈");
+}
+	
+	}
+	
