69ad66b7186710bfa013454f8bdc82ba03165b67
Danibabu121 <babudani198@gmail.com>
2025-09-01 20:21:42 +0500

    Update Asslam o Alaikum.js

--- Priyansh/commands/Asslam o Alaikum.js
+++ Priyansh/commands/Asslam o Alaikum.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
 	var name = await Users.getNameUser(event.senderID);
 	if (event.body.indexOf("Asslam")==0 || event.body.indexOf("asslam")==0 || event.body.indexOf("Aslam")==0 || event.body.indexOf("aslam")==0 || event.body.indexOf("Asslam o Alaikum")==0 || event.body.indexOf("asslam o Alaikum")==0 || event.body.indexOf("Good as")==0 || event.body.indexOf("Good As")==0 || event.body.indexOf("good Ass")==0 || event.body.indexOf("ass")==0 ) { 
 		var msg = {
-				body: `<3 Walaikum salam${name} 🤝`
+				body: `<3 وعلیکم السلام ورحمتہ اللہ وبرکاتہ ${name} 🤝`
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🤝", event.messageID, (err) => {}, true)
