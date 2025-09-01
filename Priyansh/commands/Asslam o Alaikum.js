04bd5b261ded49d23b3e13f22d26c09cb4e66796
Danibabu121 <babudani198@gmail.com>
2025-09-01 20:11:48 +0500

    Create Asslam o Alaikum.js

---
+++ Priyansh/commands/Asslam o Alaikum.js
@@ -0,0 +1,24 @@
+module.exports.config = {
+	name: "Asslam o Alaikum",
+  version: "10.0.2",
+	hasPermssion: 0,
+	credits: "Mishi", 
+	description: "Just Respond",
+	commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = async function({ api, event, client, Users, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	var name = await Users.getNameUser(event.senderID);
+	if (event.body.indexOf("Asslam")==0 || event.body.indexOf("asslam")==0 || event.body.indexOf("Aslam")==0 || event.body.indexOf("aslam")==0 || event.body.indexOf("Asslam o Alaikum")==0 || event.body.indexOf("asslam o Alaikum")==0 || event.body.indexOf("Good as")==0 || event.body.indexOf("Good As")==0 || event.body.indexOf("good Ass")==0 || event.body.indexOf("ass")==0 ) { 
+		var msg = {
+				body: `<3 Walaikum salam${name} 🤝`
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🤝", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
