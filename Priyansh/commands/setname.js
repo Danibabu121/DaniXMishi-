14f786830dfe2b8b2029b4086e2c4f6fb19e9239
Danibabu121 <babudani198@gmail.com>
2025-09-02 19:19:56 +0500

    Update setname.js

--- Priyansh/commands/setname.js
+++ Priyansh/commands/setname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "setname",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "Mishi",
 	description: "Change the nickname in your group or the person you tag",
 	commandCategory: "Box Chat",
 	usages: "[name]",
