665736b48063f592fafaa3ff7c23aaa82d23c4ee
Danibabu121 <babudani198@gmail.com>
2025-09-02 19:34:18 +0500

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -496,7 +496,7 @@ if ((event.body.toLowerCase() == "Bat suno") || (event.body.toLowerCase() == "ba
   
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `⭐💫${name}💫⭐, \n ♥️𝐃𝐀𝐍𝐈𝐒𝐇 ꭗ 𝐌𝐈𝐒𝐇𝐈♥️    ${rand}`
+      body:♥️𝐃𝐀𝐍𝐈𝐒𝐇 ꭗ 𝐌𝐈𝐒𝐇𝐈♥️ `⭐💫${name}💫⭐, \n     ${rand}`
     }
     return api.sendMessage(msg, threadID, messageID);
   };
