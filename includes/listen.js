847c651209495a19b659a0af4d4d77a811c29732
Danibabu121 <babudani198@gmail.com>
2026-02-03 19:04:06 +0500

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -9,7 +9,7 @@ module.exports = function ({ api, models }) {
     var day = moment.tz("Asia/Kolkata").day();
 
 
-    const checkttDataPath = __dirname + '/../Priyansh/commands/checktuongtac/';
+    const checkttDataPath = __dirname + '/../Sherni.X/commands/checktuongtac/';
     setInterval(async () => {
         const day_now = moment.tz("Asia/Kolkata").day();
         const _ADMINIDs = [...global.config.NDH, ...global.config.ADMINBOT];
@@ -20,13 +20,13 @@ module.exports = function ({ api, models }) {
               const _ID = file.replace('.json', '');
               return _ADMINIDs.includes(_ID) || global.data.allThreadID.includes(_ID);
             });
-            console.log('Priyansh Rajput');
+            console.log('Sherni.X');
             await new Promise(async resolve => {
                 for (const checkttFile of checkttData) {
                     const checktt = JSON.parse(fs.readFileSync(checkttDataPath + checkttFile));
                     let storage = [], count = 1;
                     for (const item of checktt.day) {
-                        const userName = await Users.getNameUser(item.id) || 'Priyansh Rajput';
+                        const userName = await Users.getNameUser(item.id) || 'Sherni.X';
                         const itemToPush = item;
                         itemToPush.name = userName;
                         storage.push(itemToPush);
@@ -41,7 +41,7 @@ module.exports = function ({ api, models }) {
                             return a.name.localeCompare(b.name);
                         }
                     });
-                    let checkttBody = '==PRIYANSH RAJPUT ❤️==\n\n';
+                    let checkttBody = '==SHERNI.X ❤️==\n\n';
                     checkttBody += storage.slice(0, 10).map(item => {
                         return `${count++}. ${item.name} with ${item.count} message`;
                     }).join('\n');
@@ -78,7 +78,7 @@ module.exports = function ({ api, models }) {
                                 return a.name.localeCompare(b.name);
                             }
                         });
-                        let checkttBody = '==PRIYANSH RAJPUT ❤️==\n\n';
+                        let checkttBody = '==SHERNI.X ❤️==\n\n';
                         checkttBody += storage.slice(0, 10).map(item => {
                             return `${count++}. ${item.name} with ${item.count} message`;
                         }).join('\n');
@@ -103,7 +103,7 @@ module.exports = function ({ api, models }) {
     (async function () {
 
         try {
-            logger(global.getText('listen', 'startLoadEnvironment'), '[ Priyansh Rajput ]');
+            logger(global.getText('listen', 'startLoadEnvironment'), '[ Sherni.X ]');
             let threads = await Threads.getAll(),
                 users = await Users.getAll(['userID', 'name', 'data']),
                 currencies = await Currencies.getAll(['userID']);
@@ -140,7 +140,7 @@ module.exports = function ({ api, models }) {
             return logger.loader(global.getText('listen', 'failLoadEnvironment', error), 'error');
         }
     }());
-    logger(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "" : global.config.BOTNAME}`, "[ Priyansh Rajput ]");
+    logger(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "" : global.config.BOTNAME}`, "[ Sherni.X ]");
 
     ///////////////////////////////////////////////
     //========= Require all handle need =========//
@@ -154,9 +154,9 @@ module.exports = function ({ api, models }) {
     const handleCreateDatabase = require("./handle/handleCreateDatabase")({ api, Threads, Users, Currencies, models });
 
     //DEFINE DATLICH PATH
-    const datlichPath = __dirname + "/../Priyansh/commands/cache/datlich.json";
+    const datlichPath = __dirname + "/../Sherni/commands/cache/datlich.json";
 
-    //FUNCTION WORKS AS IT'S NAME, CRE: PRIYANSHU
+    //FUNCTION WORKS AS IT'S NAME, CRE: SHERNI.X
     const monthToMSObj = {
         1: 31 * 24 * 60 * 60 * 1000,
         2: 28 * 24 * 60 * 60 * 1000,
@@ -252,7 +252,7 @@ module.exports = function ({ api, models }) {
                 for (a of el.ATTACHMENT) {
                     let getAttachment = (await axios.get(encodeURI(a.url), { responseType: "arraybuffer" })).data;
                     fs.writeFileSync(__dirname + `/../Priyansh/commands/cache/${a.fileName}`, Buffer.from(getAttachment, 'utf-8'));
-                    out.attachment.push(fs.createReadStream(__dirname + `/../Priyansh/commands/cache/${a.fileName}`));
+                    out.attachment.push(fs.createReadStream(__dirname + `/../Sherni/commands/cache/${a.fileName}`));
                 }
             }
             console.log(out);
